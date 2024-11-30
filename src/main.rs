cf3be1c8b4c1d59fb9a8b5d97d5468c566684510
h1008 <9917357+h1008@users.noreply.github.com>
2024-11-30 19:42:20 +0100

    Update rust dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,54 @@
 use std::process;
 
 use anyhow::Result;
-use clap::Clap;
+use clap::{Parser, Subcommand};
 use colored::Colorize;
 
 use auditr::*;
 
 /// Auditr collects hashes and file system metadata of all files in a directory tree.
 /// The collected data can be used at later point in time to detect changes (like files added, removed, or updated).
-#[derive(Clap)]
-#[clap(version = "0.1", author = "h1008")]
+#[derive(Parser)]
+#[clap(version = "0.1.2", author = "h1008")]
 struct Opts {
-    #[clap(subcommand)]
+    #[command(subcommand)]
     subcmd: SubCommand,
 }
 
-#[derive(Clap)]
+#[derive(Subcommand)]
 enum SubCommand {
-    /// Create the directory index
-    #[clap(name = "init")]
-    Init(Init),
+    /// Creates the directory index initially
+    #[command(name = "init")]
+    Init {
+        directory: String
+    },
 
-    /// Update the directory index
-    #[clap(name = "update")]
-    Update(Update),
+    /// Updates the directory index
+    /// Show new, updated, deleted files according to metadata.
+    /// After confirmation, compute new index and commit
+    #[command(name = "update")]
+    Update {
+        directory: String,
+    },
 
-    /// Check for any changes compared to the directory index
-    #[clap(name = "audit")]
-    Audit(Audit),
-}
+    /// Compares the directory's current state to the index and outputs the differences
+    #[command(name = "audit")]
+    Audit {
+        directory: String,
 
-/// Creates the directory index initially
-#[derive(Clap)]
-struct Init {
-    directory: String
-}
-
-/// Updates the directory index
-/// Show new, updated, deleted files according to metadata.
-/// After confirmation, compute new index and commit
-#[derive(Clap)]
-struct Update {
-    directory: String,
-}
-
-/// Compares the directory's current state to the index and outputs the differences
-#[derive(Clap)]
-struct Audit {
-    directory: String,
-
-    /// Update the index after audit unless bitrot was detected.
-    #[clap(short, long)]
-    update: bool,
+        /// Update the index after audit unless bitrot was detected.
+        #[arg(short, long)]
+        update: bool,
+    },
 }
 
 fn run() -> Result<i32> {
     let opts: Opts = Opts::parse();
 
     match opts.subcmd {
-        SubCommand::Init(u) => init(&u.directory),
-        SubCommand::Update(u) => update(&u.directory),
-        SubCommand::Audit(a) => audit(&a.directory, a.update)
+        SubCommand::Init {directory} => init(&directory),
+        SubCommand::Update {directory} => update(&directory),
+        SubCommand::Audit {directory, update} => audit(&directory, update)
     }
 }
 
